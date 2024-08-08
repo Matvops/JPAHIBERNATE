d90ce811b4ac961cbab9551fb4532bf78ecc3889
Matheus <matheusmartinscia@gmail.com>
2024-08-07 23:29:17 -0300

    Criei as classes order e implementei

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +23,10 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
@@ -46,6 +56,12 @@ public class User implements Serializable{
 
 	public String getPassword() {
 		return password;
+	}
+
+	
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
